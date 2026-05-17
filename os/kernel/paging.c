d4ca9b0c1b7545d530699b27a0b8029951be2324
Xtimate <lordquestionmark.discord@gmail.com>
2026-05-17 19:43:20 +0200

    rewrote paging for higher half kernel

--- os/kernel/paging.c
+++ os/kernel/paging.c
@@ -4,21 +4,28 @@
 #define PAGE_WRITABLE 0x2
 #define PAGE_USER 0x4
 
-static unsigned int page_directory[1024] __attribute__((aligned(4096)));
-static unsigned int page_table[1024] __attribute__((aligned(4096)));
+#define BOOT_PAGE_TABLE1_PHYS 0x102000
+#define BOOT_PAGE_TABLE2_PHYS 0x103000
 
-extern void paging_enable(unsigned int *page_directory);
+static unsigned int page_directory[1024] __attribute__((aligned(4096)));
+static unsigned int user_page_table[1024] __attribute__((aligned(4096)));
 
 void paging_init() {
     for (int i = 0; i < 1024; i++) {
-        page_table[i] = (i * 0x1000) | PAGE_PRESENT | PAGE_WRITABLE | PAGE_USER;
-    }
-
-    page_directory[0] = ((unsigned int)page_table) | PAGE_PRESENT | PAGE_WRITABLE | PAGE_USER;
-
-    for (int i = 1; i < 1024; i++) {
         page_directory[i] = 0;
     }
 
-    paging_enable(page_directory);
+    page_directory[768] = BOOT_PAGE_TABLE1_PHYS | PAGE_PRESENT | PAGE_WRITABLE;
+    page_directory[769] = BOOT_PAGE_TABLE2_PHYS | PAGE_PRESENT | PAGE_WRITABLE;
+
+    for (int i = 0; i < 1024; i++) {
+        user_page_table[i] = (i * 0x1000) | PAGE_PRESENT | PAGE_WRITABLE | PAGE_USER;
+    }
+
+    unsigned int user_pt_phys = (unsigned int)user_page_table - 0xC0000000;
+    page_directory[0] = user_pt_phys | PAGE_PRESENT | PAGE_WRITABLE | PAGE_USER;
+
+    unsigned int pd_phys = (unsigned int)page_directory - 0xC0000000;
+
+    __asm__ volatile ("mov %0, %%cr3" : : "r"(pd_phys));
 }
