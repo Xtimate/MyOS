5e0c2c7fe8b1ee2e18bc62e868a95470117cb048
Xtimate <lordquestionmark.discord@gmail.com>
2026-05-19 17:13:23 +0200

    added shift and ctrl handling

--- os/kernel/keyboard.c
+++ os/kernel/keyboard.c
@@ -10,12 +10,21 @@
 #define KEYBOARD_DATA_PORT 0x60
 
 static int ctrl_held = 0;
+static int shift_held = 0;
 
 static const char scancode_map[] = {
     0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0,
     0, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 0,
     0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
     0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0,
+    '*', 0, ' '
+};
+
+static const char scancode_map_shift[] = {
+    0, 0, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 0,
+    0, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', 0,
+    0, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~',
+    0, '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 0,
     '*', 0, ' '
 };
 
@@ -30,6 +39,8 @@ static void keyboard_handler(struct registers *r) {
 
     if (scancode == 0x1D) { ctrl_held = 1; return; }
     if (scancode == 0x9D) { ctrl_held = 0; return; }
+    if (scancode == 0x2A) { shift_held = 1; return; }
+    if (scancode == 0xAA) { shift_held = 0; return; }
 
     if (scancode & 0x80) return;
 
@@ -56,7 +67,9 @@ static void keyboard_handler(struct registers *r) {
     char c = 0;
     if (scancode == 0x1C) c = '\n';
     else if (scancode == 0x0E) c = '\b';
-    else if (scancode < sizeof(scancode_map)) c = scancode_map[scancode];
+    else if (scancode < sizeof(scancode_map)) {
+        c = shift_held ? scancode_map_shift[scancode] : scancode_map[scancode];
+    }
 
     if (c == 0) return;
 
